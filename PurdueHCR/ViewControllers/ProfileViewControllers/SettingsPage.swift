04b996109c8281cf9bfed436dad059d5158f3217
BenHardin <bbhardin1@gmail.com>
2018-11-06 23:31:21 -0500

    New SettingsPage ViewController File

---
+++ PurdueHCR/ViewControllers/ProfileViewControllers/SettingsPage.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsPage.swift
+//  PurdueHCR
+//
+//  Created by Benjamin Hardin on 11/6/18.
+//  Copyright © 2018 DecodeProgramming. All rights reserved.
+//
+
+import UIKit
+
+class SettingsPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
