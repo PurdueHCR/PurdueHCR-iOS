805f06c513f32288a4ea5798d40aa52d10711467
BenHardin <bbhardin1@gmail.com>
2020-01-07 18:17:14 -0500

    Working on FHP top scorers.

--- PurdueHCR/Views/TopScorersView.swift
+++ PurdueHCR/Views/TopScorersView.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class TopScorersView: UITableView {
-
-   
+class TopScorersView: UITableView, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = dequeueReusableCell(withIdentifier: "cell")
+        return cell!
+    }
+    
 
 }
