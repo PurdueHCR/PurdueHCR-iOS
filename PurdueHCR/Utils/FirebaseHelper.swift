5bd2e63a69aee91870c1e61f36024edfcb816e4f
Brennan Doyle <doyle92@purdue.edu>
2022-03-10 19:34:56 -0500

    Somehow the Production URLs got deleted so updated them to be back in the system and also activated the prod URLs on master branch so we don't forget for next update.

--- PurdueHCR/Utils/FirebaseHelper.swift
+++ PurdueHCR/Utils/FirebaseHelper.swift
@@ -40,22 +40,6 @@ class FirebaseHelper {
 //    let GET_POINT_LOG_BY_ID_URL = "http://localhost:5001/purdue-hcr-test/uscentral1/point_log/getPointLogById"
     
     //----- TEST URLS ------//
-    let CREATE_QR_LINK = "https://us-central1-purdue-hcr-test.cloudfunctions.net/link/create"
-    let HANDLE_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/point_log/handle"
-    let RANK_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/user/auth-rank"
-    let SUBMIT_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/user/submitPoint"
-    let ADD_MESSAGE_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/point_log/messages"
-    let GET_EVENT_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/event/feed"
-    let ADD_EVENT_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/event/"
-    let GRANT_AWARD_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/competition/houseAward"
-    let UPDATE_POINT_LOG_TYPE_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/point_log/updateSubmissionPointType"
-    let GET_POINT_LOG_BY_ID_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/point_log/getPointLogById"
-    let CREATE_REWARD_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/rewards/"
-    let UPDATE_REWARD_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/rewards/"
-    let DELETE_REWARD_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/rewards/"
-    
-
-//    ----- TEST URLS ------//
 //    let CREATE_QR_LINK = "https://us-central1-purdue-hcr-test.cloudfunctions.net/link/create"
 //    let HANDLE_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/point_log/handle"
 //    let RANK_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/user/auth-rank"
@@ -64,9 +48,25 @@ class FirebaseHelper {
 //    let GET_EVENT_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/event/feed"
 //    let ADD_EVENT_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/event/"
 //    let GRANT_AWARD_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/competition/houseAward"
-    //    let CREATE_REWARD_URL = ""
-    //    let UPDATE_REWARD_URL = ""
-    //    let DELETE_REWARD_URL = ""
+//    let UPDATE_POINT_LOG_TYPE_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/point_log/updateSubmissionPointType"
+//    let GET_POINT_LOG_BY_ID_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/point_log/getPointLogById"
+//    let CREATE_REWARD_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/rewards/"
+//    let UPDATE_REWARD_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/rewards/"
+//    let DELETE_REWARD_URL = "https://us-central1-purdue-hcr-test.cloudfunctions.net/rewards/"
+    
+
+//    ----- PRODUCTION URLS ------//
+    let CREATE_QR_LINK = "https://us-central1-hcr-points.cloudfunctions.net/link/create"
+    let HANDLE_URL = "https://us-central1-hcr-points.cloudfunctions.net/point_log/handle"
+    let RANK_URL = "https://us-central1-hcr-points.cloudfunctions.net/user/auth-rank"
+    let SUBMIT_URL = "https://us-central1-hcr-points.cloudfunctions.net/user/submitPoint"
+    let ADD_MESSAGE_URL = "https://us-central1-hcr-points.cloudfunctions.net/point_log/messages"
+    let GET_EVENT_URL = "https://us-central1-hcr-points.cloudfunctions.net/event/feed"
+    let ADD_EVENT_URL = "https://us-central1-hcr-points.cloudfunctions.net/event/"
+    let GRANT_AWARD_URL = "https://us-central1-hcr-points.cloudfunctions.net/competition/houseAward"
+    let CREATE_REWARD_URL = "https://us-central1-hcr-points.cloudfunctions.net/rewards/"
+    let UPDATE_REWARD_URL = "https://us-central1-hcr-points.cloudfunctions.net/rewards/"
+    let DELETE_REWARD_URL = "https://us-central1-hcr-points.cloudfunctions.net/rewards/"
     
     
     init() {
